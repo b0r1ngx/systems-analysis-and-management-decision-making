bd2b6d20571eef9ea390173e975bf4cdf19c972a
boringx <perkeboring@gmail.com>
2023-12-12 12:17:22 +0300

    1 Found that SHGO also work

---
+++ nonlinear/constraints/SimpicialHomology.py
@@ -0,0 +1,22 @@
+from scipy.optimize import shgo
+
+from nonlinear.constants import start_point
+from nonlinear.constraints.constraints import (
+    bounds, eq_bounds, radial_bounds,
+    constraints, eq_constraints, radial_constraints)
+from nonlinear.unconstraints.plotting import plot, plot_with_constraints
+from nonlinear.save_step import save_step, steps
+from nonlinear.task import f, gradient
+
+constraints = constraints
+bounds = bounds
+result = shgo(
+    func=f,
+    bounds=bounds,
+    # x0=start_point,
+    constraints=constraints,
+    callback=save_step
+)
+print(result)
+print(steps)
+plot_with_constraints(steps, constraints)
