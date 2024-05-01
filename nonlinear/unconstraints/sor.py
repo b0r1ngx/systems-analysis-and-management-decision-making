dc951d1e4cf3da9b6dfbd7a0666ca961b3dfcd87
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-05-01 11:46:08 +0300

    commented

--- nonlinear/unconstraints/sor.py
+++ nonlinear/unconstraints/sor.py
@@ -1,18 +1,20 @@
 from scipy.optimize import minimize
 
-from nonlinear.constants import start_point
-from nonlinear.unconstraints.plotting import plot
-from nonlinear.save_step import save_step, steps
-from nonlinear.task import f, gradient
-
-Broyden = 'BFGS'
-result = minimize(
-    fun=f,
-    x0=start_point,
-    method=Broyden,
-    jac=gradient,
-    callback=save_step
-)
-print(result)
-print(steps)
-plot(steps)
+# TODO: I think that SOR isn't implemented here yet.
+# TODO: Code is the same as in Broyden.py.
+# from nonlinear.constants import start_point
+# from nonlinear.unconstraints.plotting import plot
+# from nonlinear.save_step import save_step, steps
+# from nonlinear.task import f, gradient
+#
+# Broyden = 'BFGS'
+# result = minimize(
+#     fun=f,
+#     x0=start_point,
+#     method=Broyden,
+#     jac=gradient,
+#     callback=save_step
+# )
+# print(result)
+# print(steps)
+# plot(steps)
